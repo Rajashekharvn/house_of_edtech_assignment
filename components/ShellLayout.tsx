6f9d07e1985adbe000e6db13d7c071baa8762dd9
Rajashekharvn <raju.naduvinahalli@gmail.com>
2025-12-23 20:10:15 +0530

    fix: add top padding on mobile to prevent menu overlap

--- components/ShellLayout.tsx
+++ components/ShellLayout.tsx
@@ -42,7 +42,7 @@ export function ShellLayout({ children, streakCount }: ShellLayoutProps) {
                 )}
             >
                 {/* Page Content */}
-                <div className="flex-1 overflow-y-auto p-4 md:p-8">
+                <div className="flex-1 overflow-y-auto p-4 md:p-8 pt-16 md:pt-8">
                     {children}
                 </div>
             </main>
